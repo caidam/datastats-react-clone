864bf227286c518c478eb1aa3189fb97fddb9834
caidam <constant.aidam@proton.me>
2024-02-14 19:51:03 +0100

    switched to local data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 
 import React, { useState, useEffect } from "react";
 import Navbar from "./components/NavBar";
-import BarChartCall from "./components/BarChartCall";
-import LineChartCall from "./components/LineChartCall";
-import DoughnutChartCall from "./components/DoughnutChartCall";
+// import BarChartCall from "./components/BarChartCall";
+// import LineChartCall from "./components/LineChartCall";
+// import DoughnutChartCall from "./components/DoughnutChartCall";
 // import JobTitleSelector from "./components/JobTitleSelector";
 // import BarChartCall2 from "./components/BarChartCall2";
-// import BarChartLocal from "./components/local_temp/BarChartLocal";
-// import LineChartLocal from "./components/local_temp/LineChartLocal";
-// import DoughnutChartLocal from "./components/local_temp/DoughnutChartLocal";
+import BarChartLocal from "./components/local_temp/BarChartLocal";
+import LineChartLocal from "./components/local_temp/LineChartLocal";
+import DoughnutChartLocal from "./components/local_temp/DoughnutChartLocal";
 import "./test.css";
 
 function App() {
@@ -28,22 +28,22 @@ function App() {
             <div className="chart-wrapper">
                 <div className="chart-title">Line Chart - Daily Job Offers</div>
                 <div className="line-chart">
-                    <LineChartCall />
-                    {/* < LineChartLocal /> */}
+                    {/* <LineChartCall /> */}
+                    < LineChartLocal />
                 </div>
             </div>
             <div className="chart-wrapper">
                 <div className="chart-title">Bar Chart - Top Skills</div>
                 <div className="bar-chart">
-                    <BarChartCall />
-                    {/* < BarChartLocal /> */}
+                    {/* <BarChartCall /> */}
+                    < BarChartLocal />
                 </div>
             </div>
             <div className="chart-wrapper">
               <div className="chart-title"> Doughnut Chart - Offers by Job Title </div>
               <div className="doughnut-chart">
-                 <DoughnutChartCall />
-                {/* < DoughnutChartLocal /> */}
+                 {/* <DoughnutChartCall /> */}
+                < DoughnutChartLocal />
               </div>
             </div>
         </div>
